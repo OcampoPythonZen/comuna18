eb070db3803a65b79c5a55bacae1be20f5c3ba8e
OcampoPythonZen <eocampo1986@gmail.com>
2019-01-17 17:22:56 -0600

    correccion en la extraccion de los numeros telefonicos se usa xpath a cambio de selector css

--- comuna18/spiders/SAmarillaSpider.py
+++ comuna18/spiders/SAmarillaSpider.py
@@ -14,13 +14,15 @@ class SAmarillaSpider(scrapy.Spider):
         yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
+        
         for sa in response.css('div.row.l-info'):
+            cont = 1
             yield {
                 'nombre' : sa.css('span::text').extract_first(),
                 'direccion' : sa.css('div.l-address span::text').extract_first(),
-                'telefono' : sa.css('a.gtm-phone-1009925365 span::text').extract_first(),
+                'telefono' : sa.xpath('/html/body/div[6]/div[2]/div[2]/div[1]/ul/li['+str(cont)+']/div[1]/div[2]/div[2]/a/span/text()').extract_first(),
             }
+            cont += 1
         next_page = response.xpath('/html/body/div[6]/div[2]/div[3]/div/ul/li[7]/a/@href').extract_first()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
-
